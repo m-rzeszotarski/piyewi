daae7fd80e28325c3d6154635414b651246108e2
michal <m.rzeszotarski45@gmail.com>
2021-08-31 00:35:26 +0200

    pierwszy commit

---
+++ strona/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Yerba 
+
+admin.site.register(Yerba)
+
+# Register your models here.
---
+++ strona/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.utils import timezone
+
+class Yerba(models.Model):
+
+    NUMERY = [
+        (0, '0'),
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    ]
+
+    XNUMERY = [
+        (0, '0'),
+        (0.5, '0.5'),
+        (1, '1'),
+        (1.5, '1.5'),
+        (2, '2'),
+        (2.5, '2.5'),
+        (3, '3'),
+        (3.5, '3.5'),
+        (4, '4'),
+        (4.5, '4.5'),
+        (5, '5'),
+        (5.5, '5.5'),
+        (6, '6'),
+        (6.5, '6.5'),
+        (7, '7'),
+        (7.5, '7.5'),
+        (8, '8'),
+        (8.5, '8.5'),
+        (9, '9'),
+        (9.5, '9.5'),
+        (10, '10'),
+    ]
+
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    data = models.DateField(default=timezone.now)
+    firma = models.CharField(max_length=200)
+    nazwa = models.CharField(max_length=200)
+    zapylenie = models.PositiveSmallIntegerField(
+        choices=NUMERY,
+        default=0
+    )
+    aromat = models.PositiveSmallIntegerField(
+        choices=NUMERY,
+        default=0
+    )
+    moc = models.PositiveSmallIntegerField(
+        choices=NUMERY,
+        default=0
+    )
+    goryczka = models.PositiveSmallIntegerField(
+        choices=NUMERY,
+        default=0
+    )
+    nuty_owocowe = models.PositiveSmallIntegerField(
+        choices=NUMERY,
+        default=0
+    )
+    ocena = models.PositiveSmallIntegerField(
+        choices=XNUMERY,
+        default=0
+    )
+    uwagi = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nazwa
+
